b74f251db0e21ea58e137f012a0d7c0b572a19b7
Mateusz <71179061+Sygnator@users.noreply.github.com>
2021-01-17 19:09:15 +0100

    test

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { Router } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 import { createBrowserHistory } from "history";
 
 const history = createBrowserHistory();
